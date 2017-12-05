2301d7d332a31bcb19c074d9d9d863e420dc9a75
Felix Torrenegra G <f.torrenegrag@gmail.com>
2017-12-05 11:37:56 -0500

    add somethings to api and add img profile in login, show real calification of artist

--- app/js/angular_core/controllers/home.js
+++ app/js/angular_core/controllers/home.js
@@ -1,5 +1,5 @@
 angular.module("conexo")
-.controller("homeController",function ($scope,$timeout,DataBaseService,Session) {
+.controller("homeController",function ($scope,$timeout,DataBaseService,Session,$location) {
 	if (Session.isAuth()){
 		var user = Session.getUser()
 		if (user){
@@ -16,49 +16,127 @@ angular.module("conexo")
 			email: ""
 		}
 	}
-	$scope.slides = [{
-		img: "https://colombiareports.com/wp-content/uploads/2017/02/car1.x79936.jpg",
-		info: {name_a: "slide 1", ocupation: "ocupación 1"}
-	},{
-		img: "https://colombiareports.com/wp-content/uploads/2017/02/car2.x79936.jpg",
-		info: {name_a: "slide 2", ocupation: "ocupación 2"}
-	},{
-		img: "https://colombiareports.com/wp-content/uploads/2017/02/car3.x79936.jpg",
-		info: {name_a: "slide 3", ocupation: "ocupación 3"}
-	},{
-		img: "https://colombiareports.com/wp-content/uploads/2017/02/car4.x79936.jpg",
-		info: {name_a: "slide 4", ocupation: "ocupación 4"}
-	},{
-		img: "https://colombiareports.com/wp-content/uploads/2017/02/car5.x79936.jpg",
-		info: {name_a: "slide 5", ocupation: "ocupación 5"}
-	},{
-		img: "https://colombiareports.com/wp-content/uploads/2017/02/car6.x79936.jpg",
-		info: {name_a: "slide 6", ocupation: "ocupación 6"}
-	},{
-		img: "https://colombiareports.com/wp-content/uploads/2017/02/car7.x79936.jpg",
-		info: {name_a: "slide 7", ocupation: "ocupación 7"}
-	},{
-		img: "https://colombiareports.com/wp-content/uploads/2017/02/car8.x79936.jpg",
-		info: {name_a: "slide 8", ocupation: "ocupación 8"}
-	},{
-		img: "https://colombiareports.com/wp-content/uploads/2017/02/car9.x79936.jpg",
-		info: {name_a: "slide 9", ocupation: "ocupación 9"}
-	}]
+	$scope.slides = [
+		{
+			img: "https://colombiareports.com/wp-content/uploads/2017/02/car1.x79936.jpg",
+			info: {name_a: "slide 1", ocupation: "ocupación 1"}
+		},{
+			img: "https://colombiareports.com/wp-content/uploads/2017/02/car2.x79936.jpg",
+			info: {name_a: "slide 2", ocupation: "ocupación 2"}
+		},{
+			img: "https://colombiareports.com/wp-content/uploads/2017/02/car3.x79936.jpg",
+			info: {name_a: "slide 3", ocupation: "ocupación 3"}
+		},{
+			img: "https://colombiareports.com/wp-content/uploads/2017/02/car4.x79936.jpg",
+			info: {name_a: "slide 4", ocupation: "ocupación 4"}
+		},{
+			img: "https://colombiareports.com/wp-content/uploads/2017/02/car5.x79936.jpg",
+			info: {name_a: "slide 5", ocupation: "ocupación 5"}
+		},{
+			img: "https://colombiareports.com/wp-content/uploads/2017/02/car6.x79936.jpg",
+			info: {name_a: "slide 6", ocupation: "ocupación 6"}
+		},{
+			img: "https://colombiareports.com/wp-content/uploads/2017/02/car7.x79936.jpg",
+			info: {name_a: "slide 7", ocupation: "ocupación 7"}
+		},{
+			img: "https://colombiareports.com/wp-content/uploads/2017/02/car8.x79936.jpg",
+			info: {name_a: "slide 8", ocupation: "ocupación 8"}
+		},{
+			img: "https://colombiareports.com/wp-content/uploads/2017/02/car9.x79936.jpg",
+			info: {name_a: "slide 9", ocupation: "ocupación 9"}
+		}
+	]
 	$scope.categories = [
-		{id: 0, img: "http://orquestaacademicagranada.org/panel/wp-content/uploads/2012/02/musicos2.jpg", name: "Musicos", description: " descripción musicos"},
-		{id: 1, img: "http://pila.gob.ar/wp-content/uploads/2017/11/Artesanos.jpg", name: "Artesanos", description: " descripción Artesanos"},
-		{id: 2, img: "https://www.fondosypantallas.com/wp-content/uploads/2010/08/w078.jpg", name: "Bailarines", description: " descripción Bailarines"},
-		{id: 3, img: "https://imagenes.educ.ar/repositorio/Imagen/ver?image_id=dac683c8-2c4f-4421-b907-4bda73c241a2", name: "Pintores", description: " descripción Pintores"},
-		{id: 4, img: "https://marketingparafotografos.es/wp-content/uploads/2014/10/LEICA-2244x897.jpg", name: "Fotografos", description: " descripción Fotografos"},
-		{id: 5, img: "http://via.placeholder.com/350x350", name: "Cualquiera", description: " descripción cualqueira"}]
-	$scope.slide_i = 0
+        {
+        	"img": "https://i.ytimg.com/vi/oIpkUbu3ETg/maxresdefault.jpg",
+        	"name" : "Dibujo"
+        },
+        {
+        	"img": "https://imagenes.educ.ar/repositorio/Imagen/ver?image_id=dac683c8-2c4f-4421-b907-4bda73c241a2",
+        	"name" : "Pintura"
+        },
+        {
+        	"img": "http://pila.gob.ar/wp-content/uploads/2017/11/Artesanos.jpg",
+        	"name" : "Escultura"
+        },
+        {
+        	"img": "http://orquestaacademicagranada.org/panel/wp-content/uploads/2012/02/musicos2.jpg",
+        	"name" : "Música",
+            "subcategorias" : [
+                  "Clásica",
+                  "Blues",
+                  "Jazz",
+                  "R&B",
+                  "Rock and Roll",
+                  "Gospel",
+                  "Soul",
+                  "Rock",
+                  "Metal",
+                  "Punk",
+                  "Country",
+                  "Funk",
+                  "Disco",
+                  "House",
+                  "Techno",
+                  "Pop",
+                  "Ska",
+                  "Reggae",
+                  "Hip Hop",
+                  "Salsa",
+                  "Urbano",
+                  "Vallenato",
+                  "Merengue",
+                  "Ranchera",
+                  "Electrónica",
+                  "Folclor",
+                  "Boleros",
+                  "Rap"
+            ]
+        },
+        {
+        	"img": "https://www.fondosypantallas.com/wp-content/uploads/2010/08/w078.jpg",
+        	"name" : "Danza",
+        	"subcategorias" : [
+				"Ballet",
+				"Folclor",
+				"Urbano",
+				"Contemporaneo",
+				"Tango",
+				"Árabe",
+				"Salsa",
+				"Rock and Roll"
+        	]
+        },
+        {
+        	"img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Compañia_de_teatro_infantil_%22La_Colmenita_RD%22.jpg/1200px-Compañia_de_teatro_infantil_%22La_Colmenita_RD%22.jpg",
+        	"name" : "Teatro"
+        },
+        {
+        	"img": "https://educalengua.files.wordpress.com/2014/01/open_books.jpg",
+        	"name" : "Literatura"
+        },
+        {
+        	"img": "https://marketingparafotografos.es/wp-content/uploads/2014/10/LEICA-2244x897.jpg",
+        	"name" : "Audiovisuales",
+			"subcategorias" : [
+				"Cine",
+				"Fotografía",
+				"Audio",
+				"Video",
+				"Producción",
+				"Iluminación",
+			]
+        }
+    ]
 	$scope.results = []
-	$scope.propertyName = 'clasificacion'
-	$scope.reverse = true
+	$scope.slide_i = 0
 	$scope.t_order = 3
-	$scope.findCategory =  function (id) {
+	$scope.reverse = true
+	$scope.propertyName = 'clasificacion'
+
+	$scope.findCategory =  function (name) {
 		return $scope.categories.find(function (category) {
-			if (category.id == id)
+			if (category.name == name)
 				return category
 		})
 	}
@@ -92,19 +170,11 @@ angular.module("conexo")
 	$scope.searchDB = function (text) {
 		if (text != ""){
 			DataBaseService.getDB("/artist/search/"+text).success(function (data) {
-				data.forEach(function(data_i){
-					data_i.clasificacion = (Math.random() * 5).toFixed(1);
-				})
 				$scope.results = data
 			})
 		}else{
 			$scope.results = []
 		}
-		// var example = JSON.parse('{"id":"1","id_user":"4","nombre_artista":"Rancid ","foto_perfil":"https://i.ytimg.com/vi/x9B6--fvhXQ/maxresdefault.jpg","foto_portada":"URL_TO_PATH_port","categoria":"0","subcategoria":"0","facebook":"rancid","instagram":"rancid","youtube":"UCFSjnN55tV-mecyG0mYvhdQ","video":"9SCF1zbsBfU","perfil":"Cualquier texto","valor":"50000000","descuento":"10","descservicio":"Incluye- 10 temas -Equipo tecnico","estado":"1","vencimiento":null}')
-		// // example.id += $scope.results.length
-		// example.nombre_artista += example.id
-		// example.clasificacion = (Math.random() * 5).toFixed(1);
-		// $scope.results.push(example)
 	}
 	$scope.searchCategory = function (category) {
 		$scope.searchDB(category.name)
@@ -113,7 +183,7 @@ angular.module("conexo")
 	$scope.sent_login = function () {
 		Session.login($scope.login).success(function(data){
 			if (data.status != 400){
-				var user = data.user_info[0];
+				var user = {user: data.user_info[0], user_p: data.user_profile[0]};
 				Session.setUser(user)
 				$scope.current_user = user;
 				$('#modalLogin').modal('hide');
